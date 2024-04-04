15b137cf0473e2d67fee9140e16950e7818719d1
SpaYkeR669 <spayker666999@gmail.com>
2024-04-04 11:28:50 +0300

    add todo list

---
+++ todo/src/index.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+const TodoList = () => {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+
+  const addTodo = () => {
+    if (newTodo !== '') {
+      setTodos([...todos, newTodo]);
+        setNewTodo('');
+    }
+  };
+
+  const deleteTodo = (index) => {
+    const updatedTodos = todos.filter((todo, i) => i !== index);
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Add a new todo"
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
+      />
+      <button onClick={addTodo}>Add Todo</button>
+        <ul>
+          {todos.map((todo, index) => (
+            <li key={index}>
+              {todo}
+              <button onClick={() => deleteTodo(index)}>Delete</button>
+            </li>
+            ))}
+        </ul>
+    </div>
+  );
+};
+
+ReactDOM.render(<TodoList />, document.getElementById('root'));
